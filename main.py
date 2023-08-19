267e2150b00647137f44a9226ed1d72dc93a96c8
VPesherov <vyacheslav.pesherov@yandex.ru>
2023-08-19 15:57:55 +0700

    Add all

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from dotenv import load_dotenv
+import os
+from tasks.task_2.YaDiskClass import YaApiHandler
+
+
+def main():
+    # load_dotenv()
+    # ya_api_token = os.getenv('YA_API_TOKEN')
+    # user = YaApiHandler(ya_api_token)
+    # user.create_dir('test_folder')
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
